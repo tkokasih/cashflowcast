d2336a88f8da4e250baa82eff6bb3bde3a15a2c8
tkokasih <rkokasih@gmail.com>
2025-11-06 07:16:17 +0800

    fix: replace triple-slash references

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,26 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
+import 'svelte';
+import 'vite/client';
 
-declare namespace App {
-  // interface Locals {}
-  // interface PageData {}
-  // interface PageState {}
-  // interface Platform {}
+declare global {
+  namespace App {
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
-declare module '@carbon/charts-svelte' {
-  import type { SvelteComponentTyped } from 'svelte';
-
-  export type ChartTabularData = Array<Record<string, unknown>>;
-  export type LineChartOptions = Record<string, unknown>;
-
-  export class LineChart extends SvelteComponentTyped<
-    { data: ChartTabularData; options: LineChartOptions } & Record<string, unknown>
-  > {}
-}
-
-declare module '@carbon/charts' {
-  export const ScaleTypes: Record<string, string>;
-}
+export {};
